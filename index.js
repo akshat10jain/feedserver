71dae3109f8e5a57accf39da1652741469d260db
Akshat jain <akshat.jain100896@gmail.com>
2018-05-12 22:53:55 +0530

    finished auth flow

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const express=require('express');
+const express = require('express')
+const mongoose=require('mongoose')
+const cookieSession=require('cookie-session')
+const passport=require('passport')
+const keys=require('./config/keys')
+require('./models/User')
+require('./services/passport')
 
-const app=express();
+mongoose.connect(keys.mongoURI)
 
-app.get('/',(req,res)=>{
-    res.send('hello');
-})
+const app = express();
 
-const PORT=process.env.PORT || 5000
+app.use(
+    cookieSession({
+        maxAge:30*24*60*60*1000,
+        keys:[keys.cookieKey]
+    })
+)
 
-app.listen(PORT,(err)=>{
-    if(err){
+//use cookie to manage authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, (err) => {
+    if (err) {
         console.log('not started')
     }
     console.log('started')
 })
+
+
+// https://mysterious-river-82128.herokuapp.com/
